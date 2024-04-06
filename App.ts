b6bdcd3e4d964410c472345a7cef256b319950b5
Lucas Guimaraes Moreira <103656598+JkaiPrime@users.noreply.github.com>
2024-04-06 00:06:43 -0300

    Basico tipos e variaveis

---
+++ App.ts
@@ -0,0 +1,4 @@
+const soma = (n1: number,n2: number) =>{
+    return n1+n2;
+}
+console.log("Soma: " + soma(2,10))
---
+++ index.js
@@ -0,0 +1,4 @@
+const soma = (n1,n2) =>{
+    return n1+n2;
+}
+console.log("Soma: " + soma(2,10))
